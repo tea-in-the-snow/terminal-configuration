b209365bd5b682e978e82b45705140fdd96fa4fc
XiaShaoran <xiashaoran@outlook.com>
2024-01-10 00:27:56 +0800

    wezterm set pre split pane

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -59,15 +59,15 @@ config.text_background_opacity = 0.8
 -- config.command_palette_bg_color = "#333333"
 config.command_palette_bg_color = "#2F2F4F"
 
--- wezterm.on("gui-startup", function(cmd)
--- 	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
--- 	-- Create a split occupying the right 1/3 of the screen
--- 	pane:split({ size = 0.3 })
--- 	-- Create another split in the right of the remaining 2/3
--- 	-- of the space; the resultant split is in the middle
--- 	-- 1/3 of the display and has the focus.
--- 	-- pane:split { size = 0.5 }
--- end)
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+	-- Create a split occupying the right 1/3 of the screen
+	pane:split({ size = 0.75 })
+	-- Create another split in the right of the remaining 2/3
+	-- of the space; the resultant split is in the middle
+	-- 1/3 of the display and has the focus.
+	-- pane:split { size = 0.5 }
+end)
 
 config.window_padding = {
 	left = 10,
